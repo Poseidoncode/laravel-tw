e15fb2226f73a9522b1663a8bc927e68d1899683
Poseidoncode <posidomhu@gmail.com>
2025-12-09 02:03:12 +0800

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Inter } from "next/font/google";
+import {  Inter } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -24,10 +24,6 @@ const inter = Inter({
   display: "swap",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Laravel 12 中文文檔 - PHP Web 框架",
@@ -69,7 +65,7 @@ export default function RootLayout({
   return (
     <html lang="zh-TW" suppressHydrationWarning>
       <body
-        className={`${genryuSans.variable} ${inter.variable} ${geistMono.variable} antialiased`}
+        className={`${genryuSans.variable} ${inter.variable} antialiased`}
       >
         <script
           dangerouslySetInnerHTML={{
